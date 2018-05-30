51616a1cef143956a7b2f7a5003c15825ebdde53
Patryk Pilecki <ppilecki@icloud.com>
2018-05-30 12:30:21 +0100

    block class structure

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
- const ENCRYPT = require('crypto-js/512');
+const ENCRYPT = require('crypto-js/sha512');
+
+class Block {
+  constructor(index, timestamp, data, previousHash){
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+}
+exports.Block = Block;
+
+class Blockchain {
+
+}
---
+++ spec/main_spec.js
@@ -0,0 +1,9 @@
+let Block = require('../main.js');
+console.log(Block);
+
+describe('block creation', function() {
+  beforeEach(function() {
+    testBlock = new Block.Block();
+  });
+  it('returns ')
+});
