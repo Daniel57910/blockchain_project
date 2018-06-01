09789325dc7bd1cddfc1ac1f39d74e587ac4c908
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-01 15:01:54 +0100

    first routes working

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var Block = require('./lib/block.js');
+var Chain = require("./lib/blockChain.js");
+var bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.render('/index');
+  res.render('index');
 });
 
-app.listen(9000, () => console.log('Pharmacy app listening on port 9000')); 
+app.post('/info', urlencodedParser, function(req, res){
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.listen(9000, () => console.log('Pharmacy app listening on port 9000'));
