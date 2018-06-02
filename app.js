16719f04c57e6ab496850205c522f2282dc410bd
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-02 17:35:19 +0100

    routes to pages and home views for prescriptions

--- app.js
+++ app.js
@@ -11,22 +11,59 @@ app.use(express.static(path.join(__dirname, 'public')));
 var chain = new Chain.Chain();
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('home');
   console.log(chain);
 });
 
 app.get('/sign-up-patient', function (req, res) {
   res.render('sign_up_patient');
 });
+app.post('/sign-up-patient', function (req, res) {
+  res.render('sign_up_patient');
+});
+app.get('/sign-in-patient', function (req, res) {
+  res.render('sign_in_patient');
+});
+app.post('/sign-in-patient', function (req, res) {
+  res.render('sign_in_patient');
+});
 
 app.get('/sign-up-doctor', function (req, res) {
   res.render('sign_up_doctor');
+});
+app.post('/sign-up-doctor', function (req, res) {
+  res.render('sign_up_doctor');
+});
+app.get('/sign-in-doctor', function (req, res) {
+  res.render('sign_in_doctor');
+});
+app.post('/sign-in-doctor', function (req, res) {
+  res.render('sign_in_doctor');
 });
 
 app.get('/sign-up-pharmacist', function (req, res) {
   res.render('sign_up_pharmacist');
 });
+app.post('/sign-up-pharmacist', function (req, res) {
+  res.render('sign_up_pharmacist');
+});
+app.get('/sign-in-pharmacist', function (req, res) {
+  res.render('sign_in_pharmacist');
+});
+app.post('/sign-in-pharmacist', function (req, res) {
+  res.render('sign_in_pharmacist');
+});
 
+app.post('/', function (req, res) {
+  res.render('home');
+});
+
+app.post('/home', function (req, res) {
+  res.render('index');
+});
+app.get('/homepage', function (req, res) {
+  res.render('index');
+});
 app.post('/info', function(req, res){
   console.log(req.param('prescription'));
   console.log(req.param('patientNames'));
@@ -35,6 +72,7 @@ app.post('/info', function(req, res){
   console.log(newBlock);
   chain.addBlock(newBlock);
   console.log(chain);
+  res.render('info');
 });
 
 app.listen(9000, () => console.log('Pharmacy app listening on port 9000'));
