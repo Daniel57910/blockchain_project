4797e10531e4d17163b522940ea6e9b13ec051dc
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-05 16:56:00 +0100

    User login sessions beginning

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -15,6 +15,22 @@ router.get('/sign-in-doctor', function(req, res) {
   res.render('../views/doctor_views/sign_in_doctor');
 });
 
+router.post('/doctor_logged', function (req, res, next) {
+  if (req.body.fullName && req.body.password) {
+    doctorSchema.authenticate(req.body.fullName, req.body.password, function(error, doctor) {
+      if (error || !doctor) {
+        var err = new Error('Wrong name and/or password')
+        return next(err);
+      } else{
+        req.session.doctorId = doctor._id;
+        return res.redirect('/add_prescription');
+      }
+    })
+    // return next(err);
+  }
+})
+
+
 router.post('/doctor_signed', function (req, res, next) {
   if (req.body.password !== req.body.confirm_password) {
     var err = 'Password not confirmed do not match.'
@@ -36,6 +52,7 @@ router.post('/doctor_signed', function (req, res, next) {
     }
     else {
     console.log("SAVED\n" + savedDoctor);
+    req.session.doctorID = doctor._id;
     res.redirect('/add_prescription');
   };
 });
--- models/doctorModel.js
+++ models/doctorModel.js
@@ -21,6 +21,26 @@ var doctorSchema = mongoose.Schema({
   }
 });
 
+//authenticate input against database
+doctorSchema.statics.authenticate = function (fullName, password, callback) {
+  doctor.findOne({ fullName: fullName })
+  .exec(function (err, doctor) {
+    if (err) {
+      return callback(err)
+    } else if (!doctor) {
+      var err = new Error('Doctor not found. ');
+      return callback(err);
+    }
+    bcrypt.compare(password, doctor.password, function (err, result) {
+      if (result === true) {
+        return callback(null, doctor);
+      } else {
+        return callback();
+      }
+    })
+  });
+}
+
 doctorSchema.pre('save', function(next){
   var doc = this;
   bcrypt.hash(doc.password, 10, function(err, hash) {
