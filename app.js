4c89df5e087179b4462d38434df1e96322bf96ff
Daniel Miller <danielmiller5791@gmail.com>
2018-06-05 10:03:09 +0100

    prescription route renamed

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ var patientController = require('./controllers/patientController');
 var pharmacistController = require('./controllers/pharmacistController');
 
 var chain = new Chain.Chain();
+app.set('view engine', 'ejs');
+app.use(dependencies.bodyParser.urlencoded({ extended: true }));
+app.use(dependencies.express.static(dependencies.path.join(__dirname, 'public')));
+
 app.use('/', doctorController);
 app.use('/', patientController);
 app.use('/', pharmacistController);
 
 var chain = new Chain.Chain();
 dependencies.connectToDatabase(env);
-app.set('view engine', 'ejs');
-app.use(dependencies.bodyParser.urlencoded({extended: true}));
-app.use(dependencies.express.static(dependencies.path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
   res.render('home');
 });
@@ -24,11 +26,11 @@ app.get('/', function (req, res) {
 app.post('/home', function (req, res) {
   res.render('index');
 });
-app.get('/homepage', function (req, res) {
-  res.render('index');
+app.get('/add_prescription', function (req, res) {
+  res.render('add_prescription');
 });
 
-app.post('/info', function(req, res){
+app.post('/prescription_confirmation', function(req, res){
   let newBlock = new Block.Block(req.body.patientNames, req.body.doctorName, req.body.prescription);
   chain.addBlock(newBlock);
   res.render('info');
