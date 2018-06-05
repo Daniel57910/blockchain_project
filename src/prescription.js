6170e5ab6d49b08c2ee4b250e972d8909531e5fb
Sam Niechcial <samniechcial@gmail.com>
2018-06-05 12:14:26 +0100

    Prescription class written and passing first test

--- src/prescription.js
+++ src/prescription.js
@@ -1,1 +1,8 @@
-//for commmit
+class Prescription {
+  constructor(doctorName, patientName, drugsPrescribed) {
+    this.doctorName = doctorName;
+    this.patientName = patientName;
+    this.drugsPrescribed = drugsPrescribed;
+  }
+}
+module.exports = Prescription;
