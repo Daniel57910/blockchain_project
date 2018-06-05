bab85e93b38400e03c34f349bf443a3ac577de2e
Patryk Pilecki <ppilecki@icloud.com>
2018-06-05 14:23:32 +0100

    Error message if user already in database

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -15,18 +15,28 @@ router.get('/sign-in-doctor', function(req, res) {
   res.render('../views/doctor_views/sign_in_doctor');
 });
 
-router.post('/doctor_signed', function (req, res) {
+router.post('/doctor_signed', function (req, res, next) {
+  if (req.body.fullName &&
+     req.body.ID &&
+     req.body.password &&
+     req.body.confirm_password) {
   savedDoctor = new doctorSchema({
     fullName: req.body.fullName,
     doctorID: req.body.ID,
     password: req.body.password,
     confirm_password: req.body.confirm_password
   });
-  savedDoctor.save(function(err, res) {
-    if (err) throw "ERR";
+  savedDoctor.save(function(err, doctor) {
+    if (err) {
+      var err = 'user already exist'
+      return next(err)
+    }
+    else {
     console.log("SAVED\n" + savedDoctor);
-  });
-  res.redirect('/');
+    res.redirect('/');
+  };
+});
+}
 });
 
 module.exports = router;
--- src/block.js
+++ src/block.js
@@ -21,6 +21,7 @@ class Block {
     while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
              this.nonce++;
              this.hash = this.calculateHash();
+             console.log(this.hash)
     }
   }
 
