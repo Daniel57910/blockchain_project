c10d76ac59516ec4ee5cdc8b95a2533de4ed4981
Daniel Miller <danielmiller5791@gmail.com>
2018-06-05 16:10:21 +0100

    tests passing, app working on merged prescription and class branches

--- src/prescription.js
+++ src/prescription.js
@@ -2,7 +2,7 @@ class Prescription {
   constructor(doctorName, patientName, prescription) {
     this.doctorName = doctorName;
     this.patientName = patientName;
-    this.prescription = drugsPrescribed;
+    this.prescription = prescription;
   }
 }
 module.exports = Prescription;
