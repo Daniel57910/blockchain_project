3fa9f08a86ef126bf54d1a7597a4936ad3a50ca9
Daniel Miller <danielmiller5791@gmail.com>
2018-06-05 14:01:25 +0100

    logged in doctor instantiated if correct credentials added, need to create sign in for pharmacist and patient

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var router = express.Router();
 var doctorSchema = require('../models/doctorModel.js');
+var loggedInDoctor = require('../src/doctor.js');
+log = new loggedInDoctor("1", 2, 3);
+console.log(log.name);
 
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -11,16 +14,29 @@ router.get('/sign-up-doctor', function (req, res) {
   res.render('../views/doctor_views/sign_up_doctor');
 });
 
-router.get('/sign-in-doctor', function(req, res) {
-  res.render('../views/doctor_views/sign_in_doctor');
-});
-
 router.post('/doctor_signed', function (req, res) {
   saveDoctor(req);
   res.redirect('/');
 });
 
+router.get('/sign-in-doctor', function(req, res) {
+  res.render('../views/doctor_views/sign_in_doctor');
+});
+
+router.post('/doctor_logged_in', function(req, res) {
+  console.log("DATA COMING!");
+  doctorSchema.findOne({fullName: res.body.fullName}, function(err, obj) { logDoctorIn(obj);} );
+  res.redirect('/add_prescription');
+});
+
+
 module.exports = router;
+
+function logDoctorIn(obj) {
+  console.log(obj);
+  loggedInDoctor = new loggedInDoctor(obj.fullName, obj.doctorID, obj.password);
+  console.log("logged in doctor is \n" + loggedInDoctor.name);
+}
 
 function saveDoctor(req) {
   savedDoctor = new doctorSchema({
