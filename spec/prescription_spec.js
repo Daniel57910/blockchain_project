2a738b9f69147be4545723254743be81d0cbca23
Sam Niechcial <samniechcial@gmail.com>
2018-06-05 12:11:55 +0100

    First prescription test written and failing for the correct reason

---
+++ spec/prescription_spec.js
@@ -0,0 +1,14 @@
+let Prescription = require("../src/prescription.js");
+
+describe('Creating a new Pharmacist', function() {
+
+  beforeEach(function() {
+    prescription = new Prescription("Bob", "Jim", "Morphine");
+  });
+
+  it("assigns a doctor, a patient, and the drug (or drugs) to the Prescriptionon initialization", function() {
+    expect(prescription.doctorName).toEqual("Bob");
+    expect(prescription.patientName).toEqual("Jim");
+    expect(prescription.drugPrescribed).toEqual("Morphine");
+  });
+});
