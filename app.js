72735caa3c5d2397fb908c3c8e621afc59a35f4c
Patryk Pilecki <ppilecki@icloud.com>
2018-06-06 09:40:20 +0100

    next added to add_prescription route

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ app.post('/home', function (req, res) {
   res.render('index');
 });
 
-app.get('/add_prescription', function (req, res) {
+app.get('/add_prescription', function (req, res, next) {
+  console.log(req.session)
   res.render('add_prescription');
 });
 
