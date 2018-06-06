50c1eea09369dfb1a3300203455ede89c883abf1
Daniel Miller <danielmiller5791@gmail.com>
2018-06-06 15:01:56 +0100

    object mapped, problem with routing data from the form

--- controllers/prescriptionController.js
+++ controllers/prescriptionController.js
@@ -11,19 +11,30 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 router.get('/doctor/add_prescription', function (req, res) {
-  res.render('add_prescription')
+  res.render('add_prescription');
 });
 
-router.post('/prescription_confirmation', function(req, res){
+router.post('/prescription_confirmation', function(req, res, next){
   console.log(req);
   let newBlock = new Block.Block(req.body.patientName, req.body.doctorName, req.body.prescription);
   chain.addBlock(newBlock);
+  console.log("chain before is \n" + chain);
   console.log(chain);
-  res.redirect('/prescriptions/show')
+  req.session.chain = chain;
+  console.log(req.session.chain);
+  res.redirect('/prescriptions/show');
 });
-  router.get('/prescriptions/show', function(req, res){
-    // let formatted_chain = chain.join("\n")
-    console.log(chain)
-  res.render('prescription_stored')
+
+router.get('/prescriptions/show', function(req, res, next) {
+  console.log("SESSION REQUEST");
+  console.log(req.session.chain);
+  console.log("BEFORE PROCESSING");
+  formattedChain = req.session.chain.chain;
+  console.log(formattedChain);
+  data = formattedChain.map(data => data.prescription);
+  console.log("AFTER MAPPING");
+  console.log(data);
+  res.render('prescription_stored');
 });
+
 module.exports = router;
