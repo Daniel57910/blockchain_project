b449320e363f4fd3af43b1cf00db02ed63b1686d
Patryk Pilecki <ppilecki@icloud.com>
2018-06-06 12:32:25 +0100

    chainchecker unit test block.hash vs block.calcualteHash() throw error if mismatch"

--- spec/chainChecker_spec.js
+++ spec/chainChecker_spec.js
@@ -1,1 +1,29 @@
 let ChainChecker = require("../src/chainChecker.js");
+
+describe('chainChecker tests', function(){
+  beforeEach(function(){
+    function Block(previousHash = '00111111') {
+      this.previousHash = previousHash;
+      this.hash = this.calculateHash();
+      this.nonce = 0;
+    }
+    Block.prototype.calculateHash = function() {
+      return '00222222';
+    };
+    block = new Block();
+    block2 = new Block('00222222');
+
+    function Blockchain() {
+      this.chain = [block, block2];
+    }
+    chain = new Blockchain();
+    chainchecker = new ChainChecker.Chainchecker();
+  })
+
+    it('throw an error if block hash does not match block.calculateHash()', function(){
+      block2.hash = '00wrong hash'
+      expect(function() {
+        chainchecker.integrityChecker(chain);
+      }).toThrow("Chain is invalid");
+    })
+})
