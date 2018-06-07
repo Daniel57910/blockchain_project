229f3ad08846a41cfdb64f27d1e4c4f14d1723fa
Sam Niechcial <samniechcial@gmail.com>
2018-06-07 15:21:02 +0100

    home_interface tidied for whitespace and indentation

--- public/home-interface.js
+++ public/home-interface.js
@@ -1,22 +1,25 @@
- $(document).ready(function () {
+$(document).ready(function () {
   $("#doctor_sign_in").click(function() {
     window.location.href = '/doctor/sign_in';
   });
- $("#doctor_sign_up").click(function() {
-   window.location.href = '/doctor/new_registration';
- });
 
- $("#patient_sign_in").click(function() {
-   window.location.href = '/patient/sign_in';
- });
-$("#patient_sign_up").click(function() {
-  window.location.href = '/patient/new_registration';
-});
+  $("#doctor_sign_up").click(function() {
+    window.location.href = '/doctor/new_registration';
+  });
 
-$("#pharmacist_sign_in").click(function() {
-  window.location.href = '/sign-in-pharmacist';
-});
-$("#pharmacist_sign_up").click(function() {
- window.location.href = '/sign-up-pharmacist';
-});
+  $("#patient_sign_in").click(function() {
+    window.location.href = '/patient/sign_in';
+  });
+
+  $("#patient_sign_up").click(function() {
+    window.location.href = '/patient/new_registration';
+  });
+
+  $("#pharmacist_sign_in").click(function() {
+    window.location.href = '/sign-in-pharmacist';
+  });
+
+  $("#pharmacist_sign_up").click(function() {
+    window.location.href = '/sign-up-pharmacist';
+  });
 });
