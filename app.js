6041f241775ecae967089bb568ade23c7f74be0c
Daniel Miller <danielmiller5791@gmail.com>
2018-06-07 11:21:24 +0100

    app.js refactored as well

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var env = process.env.NODE_ENV || "test";
 var dependencies = require("./dependencies.js");
 var app = dependencies.setupApp();
-var Chain = require("./src/blockChain.js");
-var Block = require("./src/block.js");
+
 var doctorController = require('./controllers/doctorController');
 var patientController = require('./controllers/patientController');
 var pharmacistController = require('./controllers/pharmacistController');
-var session = require('express-session');
 var prescriptionController = require('./controllers/prescriptionController');
-var chain = new Chain.Chain();
+
+var session = require('express-session');
 app.set('view engine', 'ejs');
 app.use(dependencies.bodyParser.urlencoded({ extended: true }));
 app.use(dependencies.express.static(dependencies.path.join(__dirname, 'public')));
@@ -22,26 +21,10 @@ app.use('/', doctorController);
 app.use('/', patientController);
 app.use('/', pharmacistController);
 app.use('/', prescriptionController);
-var chain = new Chain.Chain();
 dependencies.connectToDatabase(env);
 
 app.get('/', function (req, res) {
   res.render('home');
-});
-
-app.post('/home', function (req, res) {
-  res.render('index');
-});
-
-app.get('/add_prescription', function (req, res, next) {
-  console.log(req.session)
-  res.render('add_prescription');
-});
-
-app.post('/prescription_confirmation', function(req, res){
-  let newBlock = new Block.Block(req.body.patientNames, req.body.doctorName, req.body.prescription);
-  chain.addBlock(newBlock);
-  res.render('prescription_stored');
 });
 
 module.exports = app;
